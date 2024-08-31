1bd27cd6fd39e127dbfb2083bbd91b8e9ed082e2
Chavez Guerra del cetis <159495141+ChavezGuerra2022@users.noreply.github.com>
2024-08-30 18:51:25 -0700

    30/08/2024 Diego Angel
    hola mundo php

---
+++ BasedeDatosWeb/parcial1/holamundo.php
@@ -0,0 +1,15 @@
+<?php
+    $nombre = "Luis";
+    $numero = 4;
+    $numero2 = "4";
+
+    $total = $numero + $numero2;
+
+    echo "Total =" . $total;
+    echo "<h1> HOLA MUNDO </h1";
+
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+    echo "<h3 style='color: blue;'>Diego Angel</h3>"
+?>
